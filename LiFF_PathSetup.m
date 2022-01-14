1a739178752cb13eb2decec22ec339bb567c581f
Donald Dansereau <donald.dansereau@gmail.com>
2022-01-14 17:37:55 +1100

    LiFF_PathSetup now calls vl_setup to allow running of bundled vl_feat functions

--- LiFF_PathSetup.m
+++ LiFF_PathSetup.m
@@ -1,11 +1,15 @@
 % LiFF_PathSetup.m - add the LiFF light field feature toolbox to matlab's path
 %
 % It may be convenient to add this to your startup.m file.
+%
+% Inputs:
+%   SkipVLSetup (optional) : set this to true to skip calling vl_setup from the vl_sift that ships
+%                            with LiFF. Useful when using your own version of vl_sift.
 
 % Part of LiFF Light Field Feature Toolbox v0.0.1
 % Copyright (c) 2019 Donald G. Dansereau
 
-function LiFF_PathSetup
+function LiFF_PathSetup( SkipVLSetup )
 
 if (~isdeployed)
 	% Find the path to this script, and use it as the base path
@@ -13,6 +17,14 @@ if (~isdeployed)
 	
 	fprintf('Adding path for LiFF Toolbox ');
 	addpath( fullfile(LiFFPath) );
+
+	% Optinally Call the bundled vl_setup to allow us to run vl_sift functions
+	if( ~exist( 'SkipVLSetup', 'var' ) || SkipVLSetup==false )
+		CurPath = pwd;
+		cd( fullfile(LiFFPath, 'lib','vlfeat-0.9.21','toolbox') );
+		vl_setup
+		cd( CurPath );
+	end
 	
 	fprintf('%s, done.\n', LiFF_ToolboxVersion);
 end
